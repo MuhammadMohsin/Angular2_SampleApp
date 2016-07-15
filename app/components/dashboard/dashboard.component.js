6a57e49c4131d0d0d7085c3e9d5ee6ad11ac2cb2
Mohsin <mmohsinkhalid@outlook.com>
2016-07-16 02:18:07 +0500

    Used *ngFor component by creating tweets array

--- app/components/dashboard/dashboard.component.js
+++ app/components/dashboard/dashboard.component.js
@@ -11,11 +11,20 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var dashboardComponent = (function () {
     function dashboardComponent() {
+        this.dataSet = [];
+        this.dataSet = [
+            { title: "Tweet 1", desc: "This is description of Tweet 1" },
+            { title: "Tweet 2", desc: "This is description of Tweet 2" },
+            { title: "Tweet 3", desc: "This is description of Tweet 3" },
+            { title: "Tweet 4", desc: "This is description of Tweet 4" },
+            { title: "Tweet 5", desc: "This is description of Tweet 5" },
+            { title: "Tweet 1", desc: "This is description of Tweet 6" },
+        ];
     }
     dashboardComponent = __decorate([
         core_1.Component({
             selector: 'dashboard',
-            template: '<h1>Welcome to dashboard</h1>'
+            templateUrl: 'app/components/dashboard/template.html'
         }), 
         __metadata('design:paramtypes', [])
     ], dashboardComponent);
--- app/components/dashboard/dashboard.component.ts
+++ app/components/dashboard/dashboard.component.ts
@@ -2,6 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'dashboard',
-  template: '<h1>Welcome to dashboard</h1>'
+  templateUrl: 'app/components/dashboard/template.html'
 })
-export class dashboardComponent { }
+export class dashboardComponent { 
+  dataSet : any = [];
+  constructor(){
+    this.dataSet = [
+      {title : "Tweet 1", desc : "This is description of Tweet 1"},
+      {title : "Tweet 2", desc : "This is description of Tweet 2"},
+      {title : "Tweet 3", desc : "This is description of Tweet 3"},
+      {title : "Tweet 4", desc : "This is description of Tweet 4"},
+      {title : "Tweet 5", desc : "This is description of Tweet 5"},
+      {title : "Tweet 1", desc : "This is description of Tweet 6"},
+    ]
+  }
+}
