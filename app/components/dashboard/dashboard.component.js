f9ec8c7e5ae744fb561507ee4ab994d9d21838a0
Mohsin <mmohsinkhalid@outlook.com>
2016-07-16 03:21:53 +0500

    Implemented compose new tweet feature

--- app/components/dashboard/dashboard.component.js
+++ app/components/dashboard/dashboard.component.js
@@ -11,16 +11,32 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var dashboardComponent = (function () {
     function dashboardComponent() {
+        this.tweetMsg = "";
         this.dataSet = [];
         this.dataSet = [
-            { title: "Tweet 1", desc: "This is description of Tweet 1" },
-            { title: "Tweet 2", desc: "This is description of Tweet 2" },
-            { title: "Tweet 3", desc: "This is description of Tweet 3" },
-            { title: "Tweet 4", desc: "This is description of Tweet 4" },
-            { title: "Tweet 5", desc: "This is description of Tweet 5" },
-            { title: "Tweet 1", desc: "This is description of Tweet 6" },
+            { title: "Tweet 1", desc: "This is description of Tweet 1", timestamp: new Date("1/12/15").toLocaleString() },
+            { title: "Tweet 2", desc: "This is description of Tweet 2", timestamp: new Date("2/1/15").toLocaleString() },
+            { title: "Tweet 3", desc: "This is description of Tweet 3", timestamp: new Date("3/30/15").toLocaleString() },
+            { title: "Tweet 4", desc: "This is description of Tweet 4", timestamp: new Date("4/5/15").toLocaleString() },
+            { title: "Tweet 5", desc: "This is description of Tweet 5", timestamp: new Date("5/19/15").toLocaleString() },
+            { title: "Tweet 6", desc: "This is description of Tweet 6", timestamp: new Date("6/21/15").toLocaleString() },
         ];
+        this.showCompose = false;
+        this.dataSet.reverse();
     }
+    dashboardComponent.prototype.showComposeTweet = function () {
+        this.showCompose = !this.showCompose;
+    };
+    dashboardComponent.prototype.postTweet = function () {
+        console.log("form submited:" + this.tweetMsg);
+        this.dataSet.unshift({
+            title: "Tweet " + (this.dataSet.length + 1),
+            desc: this.tweetMsg,
+            timestamp: new Date().toLocaleString()
+        });
+        this.tweetMsg = "";
+        this.showCompose = false;
+    };
     dashboardComponent = __decorate([
         core_1.Component({
             selector: 'dashboard',
